6577713dc1cadfe6a255cc1b84a5d43491f658e8
Rezzonics <rezzonics@gmail.com>
2016-06-27 23:51:13 +0200

    Add files via upload

---
+++ DMFX_1_1/lib/Release/subdir_vars.mk
@@ -0,0 +1,11 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LIB_SRCS += \
+../c55x5_drivers/lib/Release/dma_bios_drv_lib.lib \
+../c55x5_drivers/lib/Release/i2c_bios_drv_lib.lib \
+../c55x5_drivers/lib/Release/i2s_bios_drv_lib.lib 
+
+
